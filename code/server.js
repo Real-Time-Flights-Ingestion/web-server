af0ad2c8512d00ec29951efdd72d43eae5c0134a
Edoardo Fava <f.edoardo.0@gmail.com>
2024-02-01 11:23:10 +0100

    json body parsing

--- code/server.js
+++ code/server.js
@@ -23,11 +23,35 @@ const pathMatches = function () {
     }
 }()
 
+function requestBody(incomingMessage) {
+    return new Promise(function (resolve, reject) {
+        var body = ""
+        incomingMessage.on("data",
+            chunk => body += chunk
+        )
+        incomingMessage.on("end",
+            () => resolve(body)
+        )
+        incomingMessage.on("error", reject)
+    })
+}
+
 function requestListener(req, res) {
     const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress || null
     var status = 404
     if (pathMatches(req.url, settings.server.aerodataboxWebhookEndpoint)) {
         status = 200
+        requestBody(req).then(
+            function (body) {
+                try {
+                    body = JSON.parse(body)
+                    console.log("Received:", body)
+                } catch (error) {
+                    // error is instance of SyntaxError
+                    console.error("JSON parsing error:", error, body)
+                }
+            }
+        ).catch((error) => console.error("Request read error:", error))
     }
     res.writeHead(status)
     res.end()
